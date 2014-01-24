abd3a1ffcc940d26552e7a120dd76be6678d5bb7
CIII <clarence.williams@leadkarma.com>
2014-01-24 00:24:14 -0500

    Update link_checker.rb

--- lib/link_checker.rb
+++ lib/link_checker.rb
@@ -114,7 +114,7 @@ class LinkChecker
       end
     end
 
-    { :return_code => @return_code, :html_files => @html_files, :links => @links, :error => @errors, :warnings => @warnings }
+    { :return_code => @return_code, :links => @links, :error => @errors, :warnings => @warnings }
   end
 
   # Use {http://anemone.rubyforge.org Anemone} to crawl the pages at the @target URL,
